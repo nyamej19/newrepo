c52c31f93a56e0eacfff1e20b5acf0f53940c4fd
Joseph-Danquah <55623974+Joseph-Danquah@users.noreply.github.com>
2023-05-22 14:40:55 +0500

    updates

--- resources/views/chalyview/workerinfo.blade.php
+++ resources/views/chalyview/workerinfo.blade.php
@@ -3,44 +3,117 @@
 
 
 
-<section class="services">
+<main id="main">
 
-    <div class="d-flex justify-content-between align-items-center">
-        <h2 style="margin-left: 30%; margin-top:20px; margin-bottom:20px;">Worker Assesments</h2>
+    <!-- ======= Blog Section ======= -->
+    <section class="breadcrumbs">
+        <div class="container">
 
-    </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Profile And Reviews</h2>
 
-    </div>
-    <div class="container">
+                <ol>
 
-        <div class="row">
-            @foreach($workerInfos as $assesment)
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-                <div class="icon-box icon-box-cyan">
-                    <!-- <div class="icon"><i class="bx bx-file"></i></div> -->
+                    <li>Profile And Reviews</li>
+                </ol>
+            </div>
+
+        </div>
+    </section><!-- End Blog Section -->
+
+    <!-- ======= Blog Section ======= -->
+    <section id="blog" class="blog">
+        <div class="container" data-aos="fade-up">
+
+            <div class="row">
+
+                <div class="col-lg-8 entries">
+
+                    <article class="entry">
 
 
 
-                    <p class="description">
-                    <h4>Assesment {{$assesment->id}}</h4>{{$assesment->assesment}}</p>
-                    <a class="link" href="{{route('contact-reviewer' ,$assesment->user_id)}}">
+                        <div class="entry-meta">
+                            <ul>
+                            <li>
+                            <img src="{{ asset('/app/public/team-3.jpg') }}" style="width: 50px;height:50px;border-radius:50%">
+                                </li>
+                                 <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">John Doe</a></li>
 
-                        <button class="btn btn-primary">Contact the reviewer</button>
-                    </a>
+
+                            </ul>
+                        </div>
+
+                        <div class="entry-content">
+                            <p>
+                               Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam eaque alias quaerat eos veniam aliquid, quae porro voluptatum !
+                            </p>
+
+                        </div>
+
+                    </article>
+
+
+
+
+                     <article class="entry">
+
+
+
+                    <div class="entry-meta">
+                        <ul>
+                        <li>
+                        <img src="{{ asset('/app/public/team-4.jpg') }}" style="width: 50px;height:50px;border-radius:50%">
+                            </li>
+                            <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">John Doe</a></li>
+
+
+                        </ul>
+                    </div>
+
+                    <div class="entry-content">
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aliquam eaque alias quaerat eos veniam aliquid, quae porro voluptatum !
+                        </p>
+
+                    </div>
+
+                    </article>
+                </div>
+
+
+              <!-- End blog sidebar -->
+              <div class="col-lg-4">
+
+                    <div class="sidebar">
+
+                        <h3 class="sidebar-title">My Profile</h3>
+                        <div class="sidebar-item search-form">
+                        <img src="{{ asset('/app/public/team-5.jpg') }}" style="width: 70px;height:70px;border-radius:50%">
+                        </div><!-- End sidebar search formn-->
+
+                        <!-- <h3 class="sidebar-title">Categories</h3> -->
+                        <div class="sidebar-item categories">
+                            <ul>
+                                <!-- <li><a href="#">General <span>(25)</span></a></li> -->
+                                <li><a href="#">Name:John Doe </a></li>
+                                <li><a href="#">Email: johndoe@mail.com</a></li>
+                                <li><a href="#">Phone: +233501459677</a></li>
+                                <!-- <li><a href="#">CyberSecurity<span>(8)</span></a></li> -->
+                                <!-- <li><a href="#">Database Systems<span>(14)</span></a></li> -->
+                            </ul>
+                        </div><!-- End sidebar categories-->
+
+                                               </div><!-- End sidebar recent posts-->
+
+
+                    </div><!-- End sidebar -->
+
                 </div>
 
             </div>
-            @endforeach
-            @if($workerInfos == null)
-            <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
 
-
-                <button class="btn btn-success">No Assesment </button>
-
-            </div>
         </div>
-        @endif
-    </div>
+    </section><!-- End Blog Section -->
 
-    </div>
-</section>
+</main>
